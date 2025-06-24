476a82afeb1f0b8b67a678fab4a2cc98e27f6628
Talon <ak1998@live.nl>
2025-06-24 13:26:20 +0200

    GlobalExceptionHandler updated with appropriate responses. (Elaborate on applying this wherever these exceptions might occur)

--- src/main/kotlin/asd/technischleerdoel/spotitube/config/GlobalExceptionHandler.kt
+++ src/main/kotlin/asd/technischleerdoel/spotitube/config/GlobalExceptionHandler.kt
@@ -1,10 +1,13 @@
 package asd.technischleerdoel.spotitube.config
+import org.apache.coyote.BadRequestException
 import org.springframework.context.MessageSource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException.Forbidden
 import java.util.*
+import javax.naming.NotContextException
 
 @RestControllerAdvice
 class GlobalExceptionHandler(private val messageSource: MessageSource) {
@@ -15,16 +18,23 @@ class GlobalExceptionHandler(private val messageSource: MessageSource) {
     fun handleIllegalArgument(ex: IllegalArgumentException, locale: Locale): ResponseEntity<String> =
         ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
 
+    @ExceptionHandler(BadRequestException::class)
+    fun handleBadRequestError(ex: IllegalArgumentException, locale: Locale): ResponseEntity<String> =
+        ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getMessage("message.BAD_REQUEST", locale))
+
     @ExceptionHandler(NoSuchElementException::class)
     fun handleNotFound(ex: NoSuchElementException, locale: Locale): ResponseEntity<String> =
         ResponseEntity.status(HttpStatus.NOT_FOUND).body(getMessage("message.NOT_FOUND", locale))
+
+    @ExceptionHandler(Forbidden::class)
+    fun handleForbidden(ex: Forbidden, locale: Locale): ResponseEntity<String> =
+        ResponseEntity.status(HttpStatus.FORBIDDEN).body(getMessage("message.FORBIDDEN", locale))
 
     @ExceptionHandler(SecurityException::class)
     fun handleUnauthorized(ex: SecurityException, locale: Locale): ResponseEntity<String> =
         ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(getMessage("message.UNAUTHORIZED", locale))
 
-
     @ExceptionHandler(Exception::class)
     fun handleAll(ex: Exception, locale: Locale): ResponseEntity<String> =
-        ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.")
+        ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(getMessage("message.INTERNAL_SERVER_ERROR", locale))
 }
